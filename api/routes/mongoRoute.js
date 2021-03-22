9d14ba8804aadfabffff997782bc77bd0a4c65c4
Adnan Noori <noori.adnan@gmail.com>
2021-03-22 09:24:58 -0700

    - Created routes for mongoDB

---
+++ api/routes/mongoRoute.js
@@ -0,0 +1,16 @@
+const productController = require('../mongoControllers/productController.js')
+
+module.exports = (app) => {
+
+  app.route('/products')
+    .get(productController.products);
+
+  app.route('/products/:product_id')
+    .get(productController.product);
+
+  app.route('/products/:product_id/styles')
+    .get(productController.styles);
+
+  app.route('/products/:product_id/related')
+    .get(productController.related);
+}
