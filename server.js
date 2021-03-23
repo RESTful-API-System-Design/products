c4e998b402cb11093d81fd4c0c43ef87bb0c27b3
Adnan Noori <noori.adnan@gmail.com>
2021-03-23 10:05:04 -0700

    - Small testing changes

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const mongoRoutes = require('./api/routes/mongoRoute.js');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+require('newrelic');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-mySQLRoutes(app);
-// mongoRoutes(app);
+// mySQLRoutes(app);
+mongoRoutes(app);
 
 app.get('/', (req, res) => {
   res.send('hello');
@@ -19,5 +20,4 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
 
-
-
+// scp -i ./SDC.pem /Users/adnannoori/sdc/products/data/skus.csv ubuntu@ec2-18-144-47-101.us-west-1.compute.amazonaws.com:./products/data
