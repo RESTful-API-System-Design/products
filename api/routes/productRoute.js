ecbdc8a35f950acdef03b7b0395c7623814f049a
Adnan Noori <noori.adnan@gmail.com>
2021-04-02 12:12:46 -0700

    - Code cleanup

--- api/routes/productRoute.js
+++ api/routes/productRoute.js
@@ -15,31 +15,3 @@ module.exports = (app) => {
     .get(productController.related);
 }
 
-
-// SELECT * FROM features where product_Id = 111111;
-
-// SELECT
-// products.id,
-// products.name,
-// products.slogan,
-// products.slogan,
-// products.description,
-// products.category,
-// products.default_price,
-// features.feature,
-// features.value
-// FROM features
-// JOIN products
-// WHERE products.id = 1
-// AND features.product_Id = 1;
-
-// SELECT
-// id style_id,
-// name,
-// sale_price,
-// original_price,
-// default_style 'default?'
-// FROM styles
-// WHERE product_id = 1;
-
-
