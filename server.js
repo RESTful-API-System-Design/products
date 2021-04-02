5992be328580906a89857d3fbfc15a963ff4aa2b
Adnan Noori <noori.adnan@gmail.com>
2021-04-02 12:16:29 -0700

    - Code clean up

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoRoutes = require('./api/routes/mongoRoute.js');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-require('newrelic');
+// require('newrelic');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
   res.send('hello');
 });
 
-app.get('/loaderio-34d53e0a16f3b0376dd16b6db07fb425/', (req, res) => {
+app.get('/loaderio-4122854d04a5cf0a9ac8d9869e696f00/', (req, res) => {
   res.sendFile(__dirname + '/loaderio.txt');
 })
 
@@ -27,3 +27,26 @@ app.listen(port, () => {
 // scp -i ./SDC.pem /Users/adnannoori/sdc/products/data/skus.csv ubuntu@ec2-18-144-47-101.us-west-1.compute.amazonaws.com:./products/data
 
 // sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+
+
+// rs.initiate(
+//   {
+//     _id: "configrs",
+//     members: [
+//       { _id: 0, host : "13.57.205.218:27019" }
+//     ]
+//   }
+// )
+
+// mongod --replSet rs0 --port 27017 --bind_ip 0.0.0.0 --dbpath /srv/mongodb/rs0-0  --oplogSize 128
+
+// rs.initiate({
+//   _id : "rs0",
+//   members: [
+//      { _id: 0, host: "54.219.137.13" },
+//      { _id: 1, host: "3.101.36.143" },
+//      { _id: 2, host: "54.219.85.23" }
+//   ]
+// })
+
+// mongo mongodb://54.219.137.13,3.101.36.143,54.219.85.23/?replicaSet=rs0
